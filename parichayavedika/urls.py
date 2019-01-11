a87da4dc09a5a9b10f882a0f60635ab0e5eb53f4
srinisambaraju <srini.sambaraju@gmail.com>
2019-01-11 09:39:14 -0700

    Added URL's file to handle all apps views

--- parichayavedika/urls.py
+++ parichayavedika/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
-
+from django.conf.urls import url, include
 from .import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'persondetails/', include('persondetails.urls')),
     url(r'^about/$', views.about),
     url(r'^$', views.homepage)
+
 
 ]

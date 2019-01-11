9ef876232565e2327ecbbd9ffbcaa9116df56f29
srinisambaraju <srini.sambaraju@gmail.com>
2019-01-11 11:16:17 -0700

    added another comment

--- persondetails/urls.py
+++ persondetails/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from .import views
 # comment
+# adding new comments
 urlpatterns = [
 
     url(r'^$', views.person_list)
--- persondetails/views.py
+++ persondetails/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+# This method is going to display the details of the person
 def person_list(request):
     return render(request, 'persondetails/person_list.html')
